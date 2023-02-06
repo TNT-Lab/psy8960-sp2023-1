16b36dda158779e54e924e3a85dc5b882a79916e
Richard Landers <rlanders@tntlab.org>
2023-02-06 12:26:38 -0600

    week3 finished

--- project_solutions/week3/R/week3.R
+++ project_solutions/week3/R/week3.R
@@ -10,8 +10,16 @@ clean_df <- clean_df[clean_df$q6 == 1,]
 
 # Analysis
 clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, units="secs")
+hist(as.numeric(clean_df$timeSpent))
+frequency_tables_list <- lapply(clean_df[,5:14], table)
+lapply(frequency_tables_list, barplot)
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3)
+for (i in frequency_tables_list) 
+  barplot(i)
 
 
 # Alternatives
 clean_df <- raw_df[!grepl("2017-06", raw_df$timeStart),]
 clean_df$timeSpent <- as.numeric(clean_df$timeEnd - clean_df$timeStart) * 60
+for (i in 1:length(frequency_tables_list))
+  barplot(frequency_tables_list[[i]])
